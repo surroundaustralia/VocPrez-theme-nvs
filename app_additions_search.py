efeaa927ba836881393207d4922e18bbc1bb24ac
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-10-26 15:37:34 +1000

    simple / adv search all vocs passthrough to NVS

--- app_additions_search.py
+++ app_additions_search.py
@@ -1,7 +1,11 @@
 # ROUTE search
 @app.route("/search")
 def search():
-    vs = [(k.replace("http://vocab.nerc.ac.uk/collection/", "").replace("http://vocab.nerc.ac.uk/scheme/", "").replace("/current/", ""), v.title) for k, v in g.VOCABS.items()]
+    vs = [
+        (k.replace("http://vocab.nerc.ac.uk/collection/", "").replace("/current/", ""), v.title)
+        for k, v in g.VOCABS.items()
+        if v.collections == "Collection"
+    ]
     logging.info(vs)
     return render_template(
         "search.html",
