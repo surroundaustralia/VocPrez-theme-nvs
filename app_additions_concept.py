2efaf532281d7b8e4639214f431c6ff324729189
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-03 16:31:56 +1000

    handling concept versions

--- app_additions_concept.py
+++ app_additions_concept.py
@@ -4,12 +4,16 @@
 # ROUTE single concept
 @app.route("/collection/<string:vocab_id>/current/<string:concept_id>")
 @app.route("/collection/<string:vocab_id>/current/<string:concept_id>/")
-def concept(vocab_id, concept_id):
+@app.route("/collection/<string:vocab_id>/current/<string:concept_id>/<string:version_no>/")
+def concept(vocab_id, concept_id, version_no=None):
     # return redirect(
     #     url_for("object") + "?uri=http://vocab.nerc.ac.uk/collection/{}/current/{}/".format(vocab_id, concept_id)
     # )
     vocab_uri = "http://vocab.nerc.ac.uk/collection/{}/current/".format(vocab_id, concept_id)
-    concept_uri = "http://vocab.nerc.ac.uk/collection/{}/current/{}/".format(vocab_id, concept_id)
+    if version_no is not None:
+        concept_uri = "http://vocab.nerc.ac.uk/collection/{}/current/{}/{}/".format(vocab_id, concept_id, version_no)
+    else:
+        concept_uri = "http://vocab.nerc.ac.uk/collection/{}/current/{}/".format(vocab_id, concept_id)
 
     c = getattr(source, g.VOCABS[vocab_uri].source) \
         (vocab_uri, request, language=request.values.get("lang")).get_concept(concept_uri)
