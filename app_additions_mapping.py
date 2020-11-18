ffed0dc1d3dc196b809d494e4539b79f020cb43d
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-18 21:02:25 +1000

    handle internal & external mappings

--- app_additions_mapping.py
+++ app_additions_mapping.py
@@ -1,8 +1,8 @@
 
 
 # ROUTE mapping
-@app.route("/mapping/I/<string:mapping_id>/")
-def mapping(mapping_id):
+@app.route("/mapping/<int_ext>/<string:mapping_id>/")
+def mapping(int_ext, mapping_id):
     from model.mapping import MappingRenderer
     return MappingRenderer(request, mapping_id).render()
 # END ROUTE mapping
