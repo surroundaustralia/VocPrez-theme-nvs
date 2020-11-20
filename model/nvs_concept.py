5a6e3623f9891745569795bca9528496913eb96d
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-20 19:49:49 +1000

    limit SKOS view to SKOS properties only

--- model/nvs_concept.py
+++ model/nvs_concept.py
@@ -77,3 +77,59 @@ class NvsConceptRenderer(ConceptRenderer):
         return Response(
             render_template("concept.html", **_template_context), headers=self.headers
         )
+
+    def _render_skos_rdf(self):
+        g = Graph()
+        g.bind("dct", DCTERMS)
+        g.bind("skos", SKOS)
+
+        c = URIRef(self.concept.uri)
+
+        # Concept SKOS metadata
+        g.add((
+            c,
+            RDF.type,
+            SKOS.Concept
+        ))
+        g.add((
+            c,
+            SKOS.prefLabel,
+            Literal(self.concept.prefLabel, lang=config.DEFAULT_LANGUAGE)
+        ))
+        g.add((
+            c,
+            SKOS.definition,
+            Literal(self.concept.definition, lang=config.DEFAULT_LANGUAGE)
+        ))
+
+        for k, v in self.concept.related_instances.items():
+            for k2, v2 in v.items():
+                if k2 == "instances":
+                    for inst in v2:
+                        g.add((
+                            c,
+                            URIRef(k),
+                            URIRef(inst[0])  # only URIs for RDF, not prefLabels too
+                        ))
+
+        if self.concept.other_properties is not None:
+            for prop in self.concept.other_properties:
+                # SKOS & DCTERMS properties only
+                if str(prop.uri).startswith("http://www.w3.org/2004/02/skos/core#") or \
+                   str(prop.uri).startswith("http://purl.org/dc/terms/"):
+                    if str(prop.value).startswith("http"):
+                        g.add((c, URIRef(prop.uri), URIRef(prop.value)))
+                    else:
+                        g.add((c, URIRef(prop.uri), Literal(prop.value)))
+
+        # serialise in the appropriate RDF format
+        if self.mediatype in ["application/rdf+json", "application/json"]:
+            graph_text = g.serialize(format="json-ld")
+        else:
+            graph_text = g.serialize(format=self.mediatype)
+
+        return Response(
+            graph_text,
+            mimetype=self.mediatype,
+            headers=self.headers,
+        )
--- model/nvs_vocabulary.py
+++ model/nvs_vocabulary.py
@@ -107,3 +107,31 @@ class NvsVocabularyRenderer(VocabularyRenderer):
                     "prefLabel": v
                 })
         return Response(j.dumps(concepts2), mimetype="application/json", headers=self.headers)
+
+    def _render_skos_rdf(self):
+        g = Graph()
+        g.bind("skos", SKOS)
+        s = URIRef(self.vocab.uri)
+        if self.vocab.collections == "Collection":
+            g.add((s, RDF.type, SKOS.Collection))
+        else:
+            g.add((s, RDF.type, SKOS.ConceptScheme))
+        g.add((s, SKOS.prefLabel, Literal(self.vocab.title)))
+        g.add((s, SKOS.definition, Literal(self.vocab.description)))
+        # if self.vocab.hasTopConcept:
+        #     for c in self.vocab.hasTopConcept:
+        #         g.add((s, SKOS.hasTopConcept, URIRef(c[0])))
+        #         g.add((URIRef(c[0]), SKOS.prefLabel, Literal(c[1])))
+        if self.vocab.concepts:
+            for c in self.vocab.concepts:
+                if self.vocab.collections == "Collection":
+                    g.add((s, SKOS.member, URIRef(c[0])))
+                else:
+                    g.add((s, SKOS.inScheme, URIRef(c[0])))
+                g.add((URIRef(c[0]), SKOS.prefLabel, Literal(c[1])))
+
+        # serialise in the appropriate RDF format
+        if self.mediatype in ["application/rdf+json", "application/json"]:
+            return Response(g.serialize(format="json-ld"), mimetype=self.mediatype, headers=self.headers)
+        else:
+            return Response(g.serialize(format=self.mediatype), mimetype=self.mediatype, headers=self.headers)
