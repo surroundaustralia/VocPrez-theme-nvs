a841459e7e7c0cd8920afeeb50055a8d9e189dad
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-23 22:52:49 +1000

    more ABS_URI_BASE_IN_DATA issues fixed

--- app_additions_concept.py
+++ app_additions_concept.py
@@ -9,11 +9,11 @@ def concept(vocab_id, concept_id, version_no=None):
     # return redirect(
     #     url_for("object") + "?uri=http://vocab.nerc.ac.uk/collection/{}/current/{}/".format(vocab_id, concept_id)
     # )
-    vocab_uri = config.ABSOLUTE_URI_BASE + "/collection/{}/current/".format(vocab_id, concept_id)
+    vocab_uri = config.ABS_URI_BASE_IN_DATA + "/collection/{}/current/".format(vocab_id, concept_id)
     if version_no is not None:
-        concept_uri = config.ABSOLUTE_URI_BASE + "/collection/{}/current/{}/{}/".format(vocab_id, concept_id, version_no)
+        concept_uri = config.ABS_URI_BASE_IN_DATA + "/collection/{}/current/{}/{}/".format(vocab_id, concept_id, version_no)
     else:
-        concept_uri = config.ABSOLUTE_URI_BASE + "/collection/{}/current/{}/".format(vocab_id, concept_id)
+        concept_uri = config.ABS_URI_BASE_IN_DATA + "/collection/{}/current/{}/".format(vocab_id, concept_id)
 
     c = getattr(source, g.VOCABS[vocab_uri].source) \
         (vocab_uri, request, language=request.values.get("lang")).get_concept(concept_uri)
@@ -30,8 +30,8 @@ def standard_name_concept(concept_id):
     if concept_id in ["accepted", "deprecated"]:
         return standard_name(acc_dep=concept_id)
 
-    vocab_uri = config.ABSOLUTE_URI_BASE + "/standard_name/"
-    concept_uri = config.ABSOLUTE_URI_BASE + "/standard_name/{}/".format(concept_id)
+    vocab_uri = config.ABS_URI_BASE_IN_DATA + "/standard_name/"
+    concept_uri = config.ABS_URI_BASE_IN_DATA + "/standard_name/{}/".format(concept_id)
 
     c = getattr(source, g.VOCABS[vocab_uri].source) \
         (vocab_uri, request, language=request.values.get("lang")).get_concept(concept_uri)
