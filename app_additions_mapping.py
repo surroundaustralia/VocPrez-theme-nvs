efa538365c1e43395933eaa6b2f8eb4503a5eb14
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-12-01 21:34:12 +1000

    fix mapping module spelling mistake

--- app_additions_mapping.py
+++ app_additions_mapping.py
@@ -3,7 +3,7 @@
 # ROUTE mapping
 @app.route("/mapping/<int_ext>/<string:mapping_id>/")
 def mapping(int_ext, mapping_id):
-    from vocpez.model.mapping import MappingRenderer
+    from vocprez.model.mapping import MappingRenderer
     return MappingRenderer(request, int_ext, mapping_id).render()
 # END ROUTE mapping
 
